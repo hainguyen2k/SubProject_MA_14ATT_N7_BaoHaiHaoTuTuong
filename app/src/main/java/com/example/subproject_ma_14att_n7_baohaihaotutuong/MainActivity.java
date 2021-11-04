34a713fa1e252cc7c473b50f67daf947a2be4e4c
hainguyen2k <ngxuanhai1000@gmail.com>
2021-11-04 10:01:47 +0700

    Create Blank Project

---
+++ app/src/main/java/com/example/subproject_ma_14att_n7_baohaihaotutuong/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.subproject_ma_14att_n7_baohaihaotutuong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
