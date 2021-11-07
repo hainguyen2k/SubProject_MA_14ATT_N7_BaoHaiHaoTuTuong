d8fbc75f8a9174cc6bb2b8df06cb447b1a88e1d9
hainguyen2k <ngxuanhai1000@gmail.com>
2021-11-07 22:36:59 +0700

    Add demo sign in and sign up

--- app/src/main/java/com/example/subproject_ma_14att_n7_baohaihaotutuong/MainActivity.java
+++ app/src/main/java/com/example/subproject_ma_14att_n7_baohaihaotutuong/MainActivity.java
@@ -1,15 +1,73 @@
 package com.example.subproject_ma_14att_n7_baohaihaotutuong;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button btnLogin, btnSignup2;
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+
+        email= findViewById(R.id.editEmailSI);
+        password= findViewById(R.id.edtPasswordSI);
+        btnLogin= findViewById(R.id.btnLogin);
+        btnSignup2= findViewById(R.id.btnSignup2);
+        btnSignup2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+
+                if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                    Toast.makeText(MainActivity.this, "TextField is empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    auth.signInWithEmailAndPassword(txt_email, txt_password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        Intent intent = new Intent(MainActivity.this, ListConservation.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "You can't sign in", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+                }
+            }
+        });
     }
 }
