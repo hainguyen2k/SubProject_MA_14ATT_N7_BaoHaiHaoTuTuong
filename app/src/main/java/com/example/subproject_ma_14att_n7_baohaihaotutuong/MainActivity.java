930b24172cfdece06039965fe02dd8f71ef711bd
DESKTOP-1VFJKRB\USER <ngtbao2000@gmail.com>
2021-11-13 20:52:48 +0700

    Bao code finish login google

--- app/src/main/java/com/example/subproject_ma_14att_n7_baohaihaotutuong/MainActivity.java
+++ app/src/main/java/com/example/subproject_ma_14att_n7_baohaihaotutuong/MainActivity.java
@@ -1,24 +1,93 @@
 package com.example.subproject_ma_14att_n7_baohaihaotutuong;
 
-import androidx.annotation.NonNull;
+
+
+import static android.service.controls.ControlsProviderService.TAG;
+
+import android.accounts.AccountManagerFuture;
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText email, password;
+    private  int RC_SIGN_IN =1;
+    private GoogleSignInClient mGoogleSignInClient;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(acct);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+    }
+
+    private void updateUI(GoogleSignInAccount acct) {
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            updateUI(account);
+        }catch (ApiException e){
+            Log.w(TAG, "signInResult:faild code="+ e.getStatusCode());
+            updateUI(null);
+        }
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+}
+
+//code cu login fire dung co xoa nha :))
+    /*EditText email, password;
     Button btnLogin, btnSignup2;
 
     FirebaseAuth auth;
@@ -69,5 +138,4 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-}
+    }*/
